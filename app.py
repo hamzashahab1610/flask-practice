b261bc4c35b60c5b6ff68820f47f66f99f4805fd
hamzashahab1610 <hamzashahab1610@gmail.com>
2020-03-08 20:40:52 +0500

    Delete route added

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def index():
         new_content = Todo(content=task_content)
         
         try:
-            db.add(new_content)
-            db.commit()
+            db.session.add(new_content)
+            db.session.commit()
             return redirect('/')
 
         except:
@@ -34,6 +34,18 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+app.route
 
 if __name__ == "__main__":
     app.run(debug=True)
